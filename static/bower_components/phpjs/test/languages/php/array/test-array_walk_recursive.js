8b35b72370ad36a833711a1f4a2aa71c7d363b90
Chuancong Gao <chuanconggao@users.noreply.github.com>
2016-06-18 20:26:21 -0700

    Add support for GoFundMe backend; update Bower

--- static/bower_components/phpjs/test/languages/php/array/test-array_walk_recursive.js
+++ static/bower_components/phpjs/test/languages/php/array/test-array_walk_recursive.js
@@ -4,24 +4,24 @@ process.env.TZ = 'UTC'
 var expect = require('chai').expect
 var ini_set = require('../../../../src/php/info/ini_set') // eslint-disable-line no-unused-vars,camelcase
 var ini_get = require('../../../../src/php/info/ini_get') // eslint-disable-line no-unused-vars,camelcase
-var array_walk = require('../../../../src/php/array/array_walk.js') // eslint-disable-line no-unused-vars,camelcase
+var array_walk_recursive = require('../../../../src/php/array/array_walk_recursive.js') // eslint-disable-line no-unused-vars,camelcase
 
-describe('src/php/array/array_walk.js (tested in test/languages/php/array/test-array_walk.js)', function () {
+describe('src/php/array/array_walk_recursive.js (tested in test/languages/php/array/test-array_walk_recursive.js)', function () {
   it('should pass example 1', function (done) {
     var expected = true
-    var result = array_walk ([3, 4], function () {}, 'userdata')
+    var result = array_walk_recursive([3, 4], function () {}, 'userdata')
     expect(result).to.deep.equal(expected)
     done()
   })
   it('should pass example 2', function (done) {
-    var expected = false
-    var result = array_walk ('mystring', function () {})
+    var expected = true
+    var result = array_walk_recursive([3, [4]], function () {}, 'userdata')
     expect(result).to.deep.equal(expected)
     done()
   })
   it('should pass example 3', function (done) {
     var expected = true
-    var result = array_walk ({"title":"my title"}, function () {})
+    var result = array_walk_recursive([3, []], function () {}, 'userdata')
     expect(result).to.deep.equal(expected)
     done()
   })
